5c4ba8b4697df02a694b32bd1b177704b723a764
Badhon100 <badhonx102@gmail.com>
2023-06-02 00:06:07 +0600

    un-named routing is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'back.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,81 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Get x"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: (){
-                  Get.snackbar(
-                    "Message", 
-                    "This is snack abr message",
-                    snackPosition: SnackPosition.BOTTOM,
-                    colorText: Colors.red,
-                    backgroundColor: Colors.green,
-                    borderRadius: 30,
-                    margin: const EdgeInsets.all(10),
-                    animationDuration: const Duration(microseconds: 200),
-                    borderWidth: 4,
-                    borderColor: Colors.yellow,
-                    isDismissible: true,
-                    onTap: (value){
-                      print("Clicked");
-                    }
-                  );
-                }, 
-                child: const Text("Show snack message")
-              ),
-              ElevatedButton(
-                onPressed: (){
-                  Get.defaultDialog(
-                    title:  "Dialog title",
-                    titleStyle: const TextStyle(fontSize: 25),
-                    middleText: "This is middle text",
-                    middleTextStyle: const TextStyle(fontSize: 20),
-                    backgroundColor: Colors.purple,
-                    radius: 80,
-                  );
-                }, 
-                child: const Text("Dialog")
-              ),
-              ElevatedButton(
-                onPressed: (){
-                  Get.bottomSheet(
-                    Wrap(
-                      children: [
-                        ListTile(
-                          leading: const Icon(Icons.wb_sunny_outlined),
-                          title: const Text("Light theme"),
-                          onTap: (){
-                            Get.changeTheme(ThemeData.light());
-                          },                          
-                        ),
-                        ListTile(
-                          leading: const Icon(Icons.wb_sunny),
-                          title: const Text("Dark theme"),
-                          onTap: (){
-                            Get.changeTheme(ThemeData.dark());
-                          },                          
-                        ),
-                      ],
-                    ),
-                    barrierColor: Colors.amber,
-                    isDismissible: true,
-                    enableDrag: false,
-                  );
-                }, 
-                child: const Text("Bottom Sheet")
-              )
-            ],
-          ),
-        ),
-      ),
+      home: const Back()
     );
   }
 }
